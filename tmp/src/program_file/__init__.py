b59a62fdd5d71463af01605e870d4fde9bb62323
Fuyugi-LS <skysorazone.hifumimain@gmail.com>
2025-07-01 21:06:57 +0700

    refactor_data_scishell_exit

---
+++ tmp/src/program_file/__init__.py
@@ -0,0 +1,20 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                  (      o  )                                #
+#                                   \  ‾‾‾  /                                 #
+#                                    \_____/                                  #
+#                                ─── SciShell ───                             #
+#                                                                             #
+#    __init__.py                                                              #
+#                                                                             #
+#    By: Fuyugi <github.com/Fuyugi-LS>                                        #
+#                                                                             #
+#    Created: 2025/06/29 09:30:00 by Fuyugi                                   #
+#    Updated: 2025/07/06 23:00:00 by Fuyugi                                   #
+#                                                                             #
+#    © CC0 2025                                                               #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+import program_file.scishell as scishell
+import program_file.datashell as datashell
+
+__all__ = ('scishell', 'datashell')
---
+++ tmp/src/scishell/shexit.py
@@ -0,0 +1,29 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                  (      o  )                                #
+#                                   \  ‾‾‾  /                                 #
+#                                    \_____/                                  #
+#                                ─── SciShell ───                             #
+#                                                                             #
+#    exit.py                                                                  #
+#                                                                             #
+#    By: Fuyugi <github.com/Fuyugi-LS>                                        #
+#                                                                             #
+#    Created: 2025/06/29 09:30:00 by Fuyugi                                   #
+#    Updated: 2025/07/06 23:00:00 by Fuyugi                                   #
+#                                                                             #
+#    © CC0 2025                                                               #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+from program_file import scishell
+
+
+class __SERVICE__:
+    @staticmethod
+    def exit_gate() -> bool:
+        print(scishell.fetch("exit"))
+        return True
+
+
+def execute() -> bool:
+    out = __SERVICE__.exit_gate
+    return out()
